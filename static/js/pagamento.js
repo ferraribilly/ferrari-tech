3618c655b1d4e9bb397af1b04a3e0703df8c857b
ferraribilly <williamferrari52@gmail.com>
2026-04-07 17:43:52 -0300

    atualizacao14

--- static/js/pagamento.js
+++ static/js/pagamento.js
@@ -34,33 +34,6 @@ document.getElementById("btn-preference").onclick = () => {
 var usuario_Id = "{{ usuario_id }}";
 
 
-
-document.getElementById('copy').addEventListener('click', copyText);
-document.getElementById('copy-mobile').addEventListener('click', copyText);
-
-function copyText() {
-    var qrcodeText = document.getElementById('qrcode-text');
-    var copiedCodeBox = document.getElementById('copied-code');
-
-    navigator.clipboard.writeText(qrcodeText.textContent).then(function () {
-        // Efeito de blur e mostrar mensagem
-        qrcodeText.style.filter = 'blur(1px)';
-        copiedCodeBox.style.visibility = 'visible';
-        copiedCodeBox.style.opacity = '0.9';
-        copiedCodeBox.style.transition = 'all 0.3s';
-
-        setTimeout(function () {
-            qrcodeText.style.filter = '';
-            copiedCodeBox.style.visibility = '';
-            copiedCodeBox.style.opacity = '';
-        }, 5000);
-
-        // Redirecionar após copiar
-        var id = qrcodeText.dataset.id; // Se você tiver o ID armazenado no data-id
-        window.location.href = '/pendente?id=' + usuario_Id;
-    });
-}
-
 var paymentId = "{{ payment_id }}";
 var usuario_Id = "{{ usuario_id }}";
 var socket = io(window.location.origin);
